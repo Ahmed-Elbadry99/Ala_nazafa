6207aa4e405987f2abc94f92ef47cfb472835e4f
elbadry <elbadry.egypt010@gmail.com>
2025-06-30 16:05:40 +0300

    add about

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,8 @@
+const p = document.getElementById("myParagraph");
+const words = p.textContent.trim().split(/\s+/);
 
+if (words.length >= 2) {
+  words[1] = `<span>${words[1]}</span><br>`; // wrap the 2nd word
+}
+
+p.innerHTML = words.join(' ');
