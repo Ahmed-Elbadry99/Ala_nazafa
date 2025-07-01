966af91cc8dd3be348e734da2351b84d286c025a
elbadry <elbadry.egypt010@gmail.com>
2025-07-01 16:47:34 +0300

    add footer

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,89 @@ if (words.length >= 2) {
 }
 
 p.innerHTML = words.join(' ');
+
+
+
+// owl
+
+
+ $(document).ready(function () {
+    $(".owl-services").owlCarousel({
+      items: 5,
+      loop: true,
+      stagePadding: 250,
+      margin: 20,
+      responsive: true,
+      rtl:true,
+      autoplay: false,
+      autoplayTimeout: 2000,
+      nav: false,
+      dots: false,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          stagePadding: 10,
+          nav : false,
+          items: 2,
+
+        },
+        600: {
+          items: 2.5,
+          stagePadding: 0,
+          nav : false,
+
+        },
+        1024: {
+          items: 3,
+          stagePadding: 40,
+
+        },
+        1200: {
+          items: 3.5,
+          stagePadding: 50,
+
+        },
+      },
+    });
+  });
+
+
+ $(document).ready(function () {
+    $(".owl-customers").owlCarousel({
+      items: 5,
+      loop: true,
+      stagePadding: 250,
+      margin: 20,
+      responsive: true,
+      rtl:true,
+      autoplay: false,
+      autoplayTimeout: 2000,
+      nav: false,
+      dots: false,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          stagePadding: 10,
+          nav : false,
+          items: 2,
+
+        },
+        600: {
+          items: 2.5,
+          stagePadding: 0,
+          nav : false,
+
+        },
+        1024: {
+          items: 3,
+          stagePadding: 40,
+
+        },
+        1200: {
+          items: 3,
+          stagePadding: 50,
+
+        },
+      },
+    });
+  });
