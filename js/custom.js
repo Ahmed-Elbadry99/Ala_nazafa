a5053633d21a0655c306a81f1c2c2ba2e648ada4
elbadry <elbadry.egypt010@gmail.com>
2025-07-06 16:35:02 +0300

    services page

--- js/custom.js
+++ js/custom.js
@@ -19,16 +19,18 @@ if(showMenu){
 
 
 
-
 const p = document.getElementById("myParagraph");
-const words = p.textContent.trim().split(/\s+/);
+if(p){
 
-if (words.length >= 2) {
-  words[1] = `<span>${words[1]}</span><br>`; // wrap the 2nd word
-}
+  const words = p.textContent.trim().split(/\s+/);
+  
+  if (words.length >= 2) {
+    words[1] = `<span>${words[1]}</span><br>`; // wrap the 2nd word
+  }
+  
+  p.innerHTML = words.join(' ');
 
-p.innerHTML = words.join(' ');
-
+}  
 
 
 // owl
